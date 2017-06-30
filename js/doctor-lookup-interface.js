689ff55cd83963b02d558a8b15a64ca08ba513e3
Maria Del Castillo <maria.f.delcastillo@gmail.com>
2017-06-30 11:34:59 -0700

    build basic html form with correspoding js interface

--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -1,1 +1,9 @@
 var apiKey = require('./../.env').apiKey;
+
+$(document).ready(function(event){
+  $('#condition-btn').click(function(){
+    var condition = $('#condition').val();
+    $('#condition').val("");
+    $('.show-doctors').text("Your symptoms: " + condition + ".");
+  });
+});
